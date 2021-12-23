8b2ad7d5073c41e88167ed153f8375dbe933e606
CrewNew Team <71261121+crewnew-git@users.noreply.github.com>
2021-12-23 00:04:55 +0000

    Init

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// Git: https://github.com/crewnew/gspreadsheet-to-ats
+// FC: public_html/crewnew.com/projects/crewnew/gspreadsheet-to-ats
+// URL: https://projects.crewnew.com/crewnew/gspreadsheet-to-ats/
+
+$spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSXJ8gKEukzIfWlDDujG9wga9X6MryH7fRZqE5Yxng6D60PKWD1CZLIFUvgQAoVk3ro_cxesrp7bo4A/pub?gid=0&single=true&output=csv";
+
+if (!ini_set('default_socket_timeout', 15)) echo "unable to change socket timeout";
+
+$i = 0; //just to skip the first line in the CSV
+
+echo '<h2>Google Spreadsheet -> ATS (GraphQL)</h2>';
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        if ($i > 0) { //skip the first line
+            $project = $data[0];
+            $first_name = $data[1];
+            $last_name = $data[2];
+            $title = $data[3];
+            $company = $data[4];
+
+            echo $project . ' - ' . $first_name . '<br>';
+        } //end: skip the first line
+        $i++;
+    }
+    fclose($handle);
+} else
+    die("Problem reading csv");
+$conn->close();
